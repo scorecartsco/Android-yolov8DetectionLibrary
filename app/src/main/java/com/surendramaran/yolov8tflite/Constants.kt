4c9f7dd7190a11f042de413f1c67032a00be572b
muzammalabdulghafoor <muzammal@scorecarts.com>
2024-05-13 15:29:45 +0400

    initial commet

---
+++ app/src/main/java/com/surendramaran/yolov8tflite/Constants.kt
@@ -0,0 +1,8 @@
+package com.surendramaran.yolov8tflite
+
+object Constants {
+//    const val MODEL_PATH = "model.tflite"
+//    const val LABELS_PATH = "labels.txt"
+    const val MODEL_PATH = "shampoo_float32.tflite"
+    const val LABELS_PATH = "shampoo_labels.txt"
+}
